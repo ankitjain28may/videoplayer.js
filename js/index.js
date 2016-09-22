d591a0ae5213be29acd7da7b7e9325a1b1726894
ankitjain28may <ankitjain28may77@gmail.com>
2016-09-22 22:35:31 +0530

    pop up videoplayer

--- js/index.js
+++ js/index.js
@@ -1,24 +1,50 @@
-var options = "";
-var v = document.getElementById('hello');
+// Modal
+
+var htmlContent = "<div id=\"modal-videoplayer\" class=\"modal\"><div  class=\"close\" onclick=\"stop()\">&times;</div><video  tabindex=\"1\" onblur=\"stop()\" class=\"modal-content\" id=\"videoplayer\" controls><source  src=\"\" type=\"\"><source src=\"\" type=\"\">Your browser does not support the video tag.</video></div>";
+
+var modalElement="";
+var options="";
+
+// Play video on click
 $(document).ready(function() {
-  $(".clickme").click(function() {
-    if (options != "")
-      v.pause();
+  $(".videoplayer").click(function() {
+    $(htmlContent).appendTo("body");
+    modalElement = document.getElementById('videoplayer');
+
     options = JSON.parse(this.name);
-    $("#hello source").attr({
+    $("#videoplayer source").attr({
       "src": options['src'],
       "type": options['type']
     });
-    $("#myModal").modal(2000);
-    v.load();
-    v.play();
+    $("#modal-videoplayer").show();
+    modalElement.load();
+    modalElement.play();
   });
 });
 
 function stop() {
-  v.pause();
-  $("#hello source").attr({
+  modalElement.pause();
+  $("#videoplayer source").attr({
       "src": "",
       "type":""
     });
+  $("#modal-videoplayer").fadeOut("1000", function() {
+    $("#modal-videoplayer").hide();
+    $("#modal-videoplayer").remove();
+  });
 }
+
+$("#videoplayer").blur(function() {
+  console.log(10);
+  stop();
+});
+$(document).keyup(function(e) {
+  if(e.keyCode == 27)
+    stop();
+});
+$(window).on("ready resize",function() {
+  var InnerWidth = $(document).innerWidth();
+  InnerWidth-=20;
+  InnerWidth+="px";
+$("#videoplayer").css({'width':InnerWidth});
+});
